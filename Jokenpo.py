86976f390c9a02bd23d8de3d4cb8584c7e09e630
Celsiinn <celsoraphael430@gmail.com>
2021-09-28 13:44:19 -0300

    first commit

---
+++ Jokenpo.py
@@ -0,0 +1,71 @@
+import time
+from random import *
+from emoji import *
+from time import *
+
+def separador():
+    print("*----------------------------------------------------------------*")
+
+def escolhaPedra():
+    if escolhadopc == jogo[2]:
+        print(f"Voce escolheu PEDRA e eu escolhi {escolhadopc} ou seja VOCE ME VENCEU SEU CORNO ")
+    elif escolhadopc == jogo[0]:
+        print(f"Voce escolheu PEDRA e eu escolhi {escolhadopc} EMPATAMO DESGRACA")
+    else:
+        print(f"Voce escolheu PEDRA e eu escolhi {escolhadopc} PERDEU SEU TROUXA")
+
+def escolhaPapel():
+    if escolhadopc == jogo[0]:
+        print(f"Voce escolheu {jogo[1]} e eu escolhi {escolhadopc} ou seja VOCE ME VENCEU SEU CORNO ")
+    elif escolhadopc == jogo[1]:
+        print(f"Voce escolheu {jogo[1]} e eu escolhi {escolhadopc} EMPATAMO DESGRACA")
+    else:
+        print(f"Voce escolheu {jogo[1]} e eu escolhi {escolhadopc} PERDEU SEU TROUXA")
+
+def escolhatesoura():
+    if escolhadopc == jogo[1]:
+        print(f"Voce escolheu {jogo[2]} e eu escolhi {escolhadopc} ou seja VOCE ME VENCEU SEU CORNO ")
+    elif escolhadopc == jogo[2]:
+        print(f"Voce escolheu {jogo[2]} e eu escolhi {escolhadopc} EMPATAMO DESGRACA")
+    else:
+        print(f"Voce escolheu {jogo[2]} e eu escolhi {escolhadopc} PERDEU SEU TROUXA")
+
+
+
+
+print("")
+print("---------------------------------*Bem Vindo ao Pedra , Papel , Tesoura-------------------------*")
+print(emojize("Escolha :\n1 - Pedra :rock: \n2 - Papel :page_facing_up: \n3 - Tesoura :scissors:" , use_aliases= True))
+separador()
+escolha = int(input("Qual sua escolha"))
+
+
+jogo = ["PEDRA" , "PAPEL" , "TESOURA"]
+escolhadopc=choice(jogo)
+if escolha == 1:
+    separador()
+    print("Estou pensando....")
+    sleep(5)
+    escolhaPedra()
+
+elif escolha == 2:
+    separador()
+    print("Estou pensando....")
+    separador()
+    sleep(5)
+    escolhaPapel()
+else:
+    separador()
+    print("Estou pensando....")
+    separador()
+    sleep(5)
+
+    escolhatesoura()
+
+
+
+
+
+
+
+
